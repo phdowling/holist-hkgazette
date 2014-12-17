19e7f249336c946c5d4233f7526b3ccd22c39203
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-12-17 17:37:10 +0800

    added Calaca

---
+++ web/Calaca/js/config.js
@@ -0,0 +1,17 @@
+/*
+ * Calaca - Search UI for Elasticsearch
+ * https://github.com/romansanchez/Calaca
+ * http://romansanchez.me
+ * @rooomansanchez
+ * 
+ * v1.1.1
+ * MIT License
+ */
+
+/* Configs */
+var indexName = "holist-gazette.new_documents"; //Ex: twitter
+var docType = "string"; //Ex: tweet
+var maxResultsSize = 30;
+var host = "http://lamma.jmsc.hku.hk/"; //Ex: http://ec2-123-aws.com
+var port = 9200;
+var protocol = ""; //Default: same as browser
