5a86a7ca9c35dc8c72e43f9b271f1422f3947beb
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-12-17 17:40:12 +0800

    added Calaca

--- web/Calaca/js/config.js
+++ web/Calaca/js/config.js
@@ -12,6 +12,6 @@
 var indexName = "holist-gazette.new_documents"; //Ex: twitter
 var docType = "string"; //Ex: tweet
 var maxResultsSize = 30;
-var host = "http://lamma.jmsc.hku.hk"; //Ex: http://ec2-123-aws.com
+var host = "lamma.jmsc.hku.hk"; //Ex: http://ec2-123-aws.com
 var port = 9200;
 var protocol = ""; //Default: same as browser
