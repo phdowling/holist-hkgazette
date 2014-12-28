5db7239acaff7efe52b237ff72fecc0c9014af23
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-12-28 22:00:55 +0100

    rename calaca folder to holist, for serving

---
+++ web/holist/js/config.js
@@ -0,0 +1,17 @@
+/*
+ * Calaca - Search UI for Elasticsearch
+ * https://github.com/romansanchez/Calaca
+ * http://romansanchez.me
+ * @rooomansanchez
+ * 
+ * v1.1.1
+ * MIT License
+ */
+
+/* Configs */
+var indexName = "search"; //Ex: twitter
+var docType = "subject_text"; //Ex: tweet
+var maxResultsSize = 30;
+var host = "lamma.jmsc.hku.hk"; //Ex: http://ec2-123-aws.com
+var port = 8082; //9200;
+var protocol = ""; //Default: same as browser
